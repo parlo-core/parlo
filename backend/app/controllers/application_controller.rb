9c59eb31102772a1fcd796447601267d191b8405
Lovro Colic <lovro.colic.lc@gmail.com>
2024-05-10 23:29:15 +0200

    add basic health route

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  def health_status
+    begin
+      ActiveRecord::Base.connection.execute('')
+      render(
+        json: {
+          message: 'Healthy'
+        },
+        status: :ok,
+      )
+    rescue ActiveRecord::ActiveRecordError => e
+      render(
+        json: {
+          message: 'Sick',
+          details: e.message,
+        },
+        status: :internal_server_error,
+      )
+    end
+  end
 end
