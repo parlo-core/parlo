6177b7a0e7d5e1503fb2e2fa5d20b9b8d4863df4
Lovro Colic <lovro.colic.lc@gmail.com>
2024-06-20 09:04:04 +0200

    add user model and specs

---
+++ backend/spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { described_class.new(email: 'test@example.com', password: 'Example1!') }
+
+  describe 'Validations' do
+    it 'passes validations if all required attributes are given' do
+      expect(subject).to be_valid
+    end
+
+    it 'does not pass validations if the is no email' do
+      subject.email = nil
+
+      expect(subject).not_to be_valid
+    end
+
+    it 'does not pass validations if the is no password' do
+      subject.password = nil
+
+      expect(subject).not_to be_valid
+    end
+  end
+end
