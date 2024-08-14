c5f13860117415c7b261e7f1d33f030130a0a4b9
Lovro Colic <lovro.colic.lc@gmail.com>
2024-08-14 09:33:49 +0200

    add model specs

---
+++ backend/spec/models/list_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe List, type: :model do
+  subject { described_class.new(name: 'Test List') }
+
+  describe 'Validations' do
+    it 'passes validations if all required attributes are given' do
+      expect(subject).to be_valid
+    end
+
+    it 'does not pass validations if there is no name' do
+      subject.name = nil
+
+      expect(subject).not_to be_valid
+    end
+  end
+end
