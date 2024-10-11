334abe388a690554fcd5cb5b29e4cd85f0f31c1f
ivonabrajdic <ivona.brajdic96@gmail.com>
2024-10-11 12:41:01 +0200

    Implement login and signup on frontend

--- frontend/src/authProvider.ts
+++ frontend/src/authProvider.ts
@@ -1,84 +1,124 @@
-import { AuthBindings } from "@refinedev/core";
+import { AuthBindings } from "@refinedev/core"
 
-export const TOKEN_KEY = "refine-auth";
+export const TOKEN_KEY = "refine-auth"
+const apiUrl = import.meta.env.VITE_API_BASE_URL
 
 export const authProvider: AuthBindings = {
   register: async ({ firstName, lastName, email, password, companyName }) => {
-    // For now, we didn't send a request, and we save the token in localStorage.
-    // In real world, we will send a request and the token will be saved in a more secure place.
-    localStorage.setItem("token", email);
-    alert("You have successfully completed sign up!");
-    return {
-      success: true,
-    };
-  },
-  login: async ({ email, password }) => {
-    // TODO: handle login
-    // const {status} = handleLogin(email, password);
-    //
-    // if (status === 200) {
-    //   return { success: true, redirectTo: "/dashboard" };
-    // } else {
-    //   return {
-    //     success: false,
-    //     error: {
-    //       name: "LoginError",
-    //       message: "Invalid username or password",
-    //     },
-    //   };
-    // }
+    try {
+      const response = await fetch(`${apiUrl}admin/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          user: {
+            first_name: firstName,
+            last_name: lastName,
+            email,
+            password,
+            company_name: companyName
+          }
+        })
+      })
 
-    // TODO: remove this block
-    if (email && password) {
-      localStorage.setItem(TOKEN_KEY, email);
+      if (!response.ok) {
+        const errorData = await response.json()
+        return {
+          success: false,
+          error: {
+            name: "RegistrationError",
+            message: errorData.message || "Failed to register"
+          }
+        }
+      }
+
+      const data = await response.json()
+      const { user } = data
+
+      localStorage.setItem(TOKEN_KEY, user.token)
+
       return {
         success: true,
-        redirectTo: "/",
-      };
+        redirectTo: "/"
+      }
+    } catch (error) {
+      return {
+        success: false,
+        error: {
+          name: "RegistrationError",
+          message: "Failed to register due to an unexpected error"
+        }
+      }
+    }
+  },
+  login: async ({ email, password }) => {
+    const response = await fetch(`${apiUrl}admin/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user: {
+          email,
+          password
+        }
+      })
+    })
+
+    if (!response.ok) {
+      const errorData = await response.json()
+      return {
+        success: false,
+        error: {
+          name: "LoginError",
+          message: errorData.message || "Failed to login"
+        }
+      }
     }
 
-    return {
-      success: false,
-      error: {
-        name: "LoginError",
-        message: "Invalid username or password",
-      },
-    };
-  },
-  logout: async () => {
-    localStorage.removeItem(TOKEN_KEY);
+    const data = await response.json()
+    const { user } = data
+    localStorage.setItem(TOKEN_KEY, user.token)
     return {
       success: true,
-      redirectTo: "/login",
-    };
+      redirectTo: "/"
+    }
+  },
+  logout: async () => {
+    localStorage.removeItem(TOKEN_KEY)
+    return {
+      success: true,
+      redirectTo: "/login"
+    }
   },
   check: async () => {
-    const token = localStorage.getItem(TOKEN_KEY);
+    const token = localStorage.getItem(TOKEN_KEY)
     if (token) {
       return {
-        authenticated: true,
-      };
+        authenticated: true
+      }
     }
 
     return {
       authenticated: false,
-      redirectTo: "/login",
-    };
+      redirectTo: "/login"
+    }
   },
   getPermissions: async () => null,
   getIdentity: async () => {
-    const token = localStorage.getItem(TOKEN_KEY);
+    const token = localStorage.getItem(TOKEN_KEY)
     if (token) {
       return {
         id: 1,
         name: "John Doe",
-        avatar: "https://i.pravatar.cc/300",
-      };
+        avatar: "https://i.pravatar.cc/300"
+      }
     }
-    return null;
+    return null
   },
   onError: async (error) => {
-    console.error(error);
-    return { error };
-  },
-};
+    console.error(error)
+    return { error }
+  }
+}
